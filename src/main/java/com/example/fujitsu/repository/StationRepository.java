0bb156fa11d7d603036b61a2076fc0c0aa18ab12
lastovkoi <lastovkoi@gmail.com>
2024-03-30 18:58:16 +0200

    Added ATEF (air temperature extra fee) calculation method

--- src/main/java/com/example/fujitsu/repository/StationRepository.java
+++ src/main/java/com/example/fujitsu/repository/StationRepository.java
@@ -4,4 +4,5 @@ import com.example.fujitsu.model.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StationRepository extends JpaRepository<Station, Integer> {
+    Station findTopByNameContainingIgnoreCaseOrderByIdDesc(String name);
 }
--- src/main/java/com/example/fujitsu/service/StationService.java
+++ src/main/java/com/example/fujitsu/service/StationService.java
@@ -7,9 +7,7 @@ import com.example.fujitsu.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -18,11 +16,20 @@ public class StationService {
     private final StationRepository stationRepository;
     private final StationMapper stationMapper;
 
-    private static final HashMap<String, Double> CITY_VEHICLE_PRICE_MAP = new HashMap<>(Map.of(
+    private static final Map<String, Double> RBF_VEHICLE_CITY_PRICE_MAP = new HashMap<>(Map.of(
         "tallinn car", 4.0, "tallinn scooter", 3.5, "tallinn bike", 3.0,
         "tartu car", 3.5, "tartu scooter", 3.0, "tartu bike", 2.5,
         "pärnu car", 3.0, "pärnu scooter", 2.5, "pärnu bike", 2.0
     ));
+
+    private static final Set<String> ATEF_VEHICLES = new HashSet<>(Set.of("Scooter", "Bike"));
+    private static final double LOW_TEMP_THRESHOLD = -10;
+    private static final double LOW_TEMP_FEE = 1.0;
+    private static final double MID_TEMP_THRESHOLD = 0;
+    private static final double MID_TEMP_FEE = 0.5;
+
+    private static final Set<String> WSEF_VEHICLES = new HashSet<>(Set.of("Bike"));
+    private static final Set<String> WPEF_VEHICLES = new HashSet<>(Set.of("Scooter", "Bike"));
 
     public List<StationDto> getStations() {
         List<Station> stations = stationRepository.findAll();
@@ -35,22 +42,40 @@ public class StationService {
     }
 
     public double calculateFee(String city, String vehicle) {
+        city = city.toLowerCase();
+        vehicle = vehicle.toLowerCase();
+
         double fee = calculateRBF(city, vehicle);
-        fee += calculateATEF();
-        fee += calculateWSEF();
-        fee += calculateWPEF();
+
+        Station station = stationRepository.findTopByNameContainingIgnoreCaseOrderByIdDesc(city);
+        if (ATEF_VEHICLES.contains(vehicle)) {
+            fee += calculateATEF(station.getAirTemperature());
+        }
+        if (WSEF_VEHICLES.contains(vehicle)) {
+            fee += calculateWSEF();
+        }
+        if (WPEF_VEHICLES.contains(vehicle)) {
+            fee += calculateWPEF();
+        }
+
         return fee;
     }
 
     private double calculateRBF(String city, String vehicle) {
-        String cityVehicle = (city + " " + vehicle).toLowerCase();
-        if (CITY_VEHICLE_PRICE_MAP.containsKey(cityVehicle)) {
-            return CITY_VEHICLE_PRICE_MAP.get(cityVehicle);
+        String cityVehicle = city + " " + vehicle;
+        if (RBF_VEHICLE_CITY_PRICE_MAP.containsKey(cityVehicle)) {
+            return RBF_VEHICLE_CITY_PRICE_MAP.get(cityVehicle);
         }
         return 0;
     }
 
-    private double calculateATEF() {
+    private double calculateATEF(double airTemperature) {
+        if (airTemperature < LOW_TEMP_THRESHOLD) {
+            return LOW_TEMP_FEE;
+        }
+        else if (airTemperature >= LOW_TEMP_THRESHOLD && airTemperature < MID_TEMP_THRESHOLD) {
+            return MID_TEMP_FEE;
+        }
         return 0;
     }
 
